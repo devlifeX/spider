54866117fd8fb30381cfa0586f22946659be82e8
dariushvesal <dariush.vesal@gmail.com>
2021-09-15 23:42:22 +0430

    Ad cheerio

--- src/lib/url-status/index.ts
+++ src/lib/url-status/index.ts
@@ -1,6 +1,28 @@
 import axios from "axios";
 import * as t from "exectimer";
+import cheerio from "cheerio";
+
 import { splitEvery, flatten } from "ramda";
+
+const seoReport = (html: string) => {
+  const $ = cheerio.load(html);
+
+  return {
+    h1: {
+      text: $("h1").text(),
+      count: $("h1").length,
+    },
+    title: {
+      text: $("title").text(),
+      good: $("title").text().length < 64,
+    },
+    description: $("meta[name='description']").attr("content"),
+    hasViewport: $("meta[name='viewport']").length > 0,
+    isIndexable: $("meta[name='robots']").attr("content").includes("index"),
+    hasValidCanonical: $("link[rel='canonical']").attr("href").includes("http"),
+    image: $("meta[property='og:image']").attr("content"),
+  };
+};
 
 const makeHttpCall = async (url: string) => {
   const id = Buffer.from(url).toString("base64");
@@ -23,6 +45,9 @@ const makeHttpCall = async (url: string) => {
       status,
       statusText,
       bodySize: res.data.length,
+      seo: {
+        ...seoReport(res.data),
+      },
     };
   });
 };
