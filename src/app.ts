a1c5b764d008a710d2bab890af2a0f6fd598eaae
Dariush Vesal <dariush.vesal@gmail.com>
2024-11-17 23:46:40 +0330

    Fix

--- src/app.ts
+++ src/app.ts
@@ -9,23 +9,24 @@ import { Server } from "socket.io";
 import { socketConnect } from "./socket";
 
 const app: express.Express = express();
-connectToDb(config.connectionString).then(() => {
-  app.use(cors(config.corsOptions));
-  app.use(express.json());
-  app.use(failResponseHandler);
-  app.use(successResponseHandler);
+// connectToDb(config.connectionString).then(() => {
+app.use(cors(config.corsOptions));
+app.use(express.json());
+app.use(failResponseHandler);
+app.use(successResponseHandler);
 
-  const httpServer = createServer(app);
-  const io = new Server(httpServer, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
-  socketConnect(io);
-  httpServer.listen(config.port, () => {
-    console.log(`Socket listening on the port ${config.port}`);
-  });
-
-  registerRoutes(app);
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
+socketConnect(io);
+
+httpServer.listen(config.port, () => {
+  console.log(`Socket listening on the port ${config.port}`);
+});
+
+registerRoutes(app);
+// });
--- src/config/index.ts
+++ src/config/index.ts
@@ -11,10 +11,10 @@ const corsOptions: CORS = {
 
 export default {
   currentEnv: process.env.NODE_ENV || "development",
-  port: process.env.PORT,
-  socketPort: process.env.SOCKET_PORT,
-  connectionString: process.env.CONNECTION_STRING,
-  apiSecret: process.env.API_SECRET,
+  port: process.env.PORT || 3000,
+  socketPort: process.env.SOCKET_PORT || 3003,
+  connectionString: process.env.CONNECTION_STRING || "",
+  apiSecret: process.env.API_SECRET || "",
   corsOptions,
   DIR_SPIDER: path.resolve(__dirname, "../.."),
 };
